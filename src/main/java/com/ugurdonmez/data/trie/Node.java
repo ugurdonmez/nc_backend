d8999a054b3fe11c3de769367ed31f496793350a
ugurdonmez <ugurdonmez87@gmail.com>
2016-12-05 01:18:17 +0100

    trie added.

---
+++ src/main/java/com/ugurdonmez/data/trie/Node.java
@@ -0,0 +1,73 @@
+package com.ugurdonmez.data.trie;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Trie node implementation
+ * 
+ * ref: https://github.com/ugurdonmez/GoogleQuestions/tree/master/src/common/trie
+ * 
+ * Created by ugurdonmez on 30/07/16.
+ */
+protected class Node {
+
+    private final char c;
+    private Map<Character, Node> children;
+    private boolean isLeaf;
+
+    public Node(char c) {
+        this.c = c;
+        this.isLeaf = false;
+        this.children = new TreeMap<Character, Node>();
+    }
+
+    public boolean isIsLeaf() {
+        return isLeaf;
+    }
+
+    public Map<Character, Node> getChildren() {
+        return children;
+    }
+
+    public Node getByChar(Character c) {
+        return children.get(c);
+    }
+
+    public char getC() {
+        return c;
+    }
+
+    public void addWord (String str, int level) {
+        if (level == str.length()) {
+            this.isLeaf = true;
+            return;
+        }
+
+        char c = str.charAt(level);
+
+        if (!children.containsKey(c)) {
+            children.put(c, new Node(c));
+        }
+
+        children.get(c).addWord(str, level+1);
+    }
+
+    public ArrayList<String> getWords(String str) {
+        ArrayList<String> list = new ArrayList<String>();
+
+        if (this.isLeaf) {
+            list.add(str);
+        }
+
+        Iterator<Character> it = this.children.keySet().iterator();
+
+        while (it.hasNext()) {
+            list.addAll(children.get(it.next()).getWords(str+c));
+        }
+
+        return list;
+    }
+}
---
+++ src/main/java/com/ugurdonmez/data/trie/Trie.java
@@ -0,0 +1,74 @@
+package com.ugurdonmez.data.trie;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Trie implementation
+ * 
+ * ref: https://github.com/ugurdonmez/GoogleQuestions/tree/master/src/common/trie
+ * 
+ * Created by ugurdonmez on 30/07/16.
+ */
+public class Trie {
+
+    private Node head;
+
+    public Trie() {
+        this.head = new Node(' ');
+    }
+
+    public void addWord(String str) {
+        this.head.addWord(str, 0);
+    }
+
+    /**
+     * Autocomplete
+     * 
+     * @param str
+     * @return
+     */
+    public ArrayList<String> getWord(String str) {
+
+        try {
+            Node node = this.head;
+            for (char c : str.toCharArray()) {
+                node = node.getChildren().get(c);
+            }
+
+            ArrayList<String> list = new ArrayList<String>();
+
+            Iterator<Character> it = node.getChildren().keySet().iterator();
+
+            while (it.hasNext()) {
+                list.addAll(node.getChildren().get(it.next()).getWords(str));
+            }
+
+            return list;
+        } catch (NullPointerException ex) {
+            return null;
+        }
+    }
+
+    /**
+     * return true when trie include given string
+     * 
+     * @param str
+     * @return
+     */
+    public boolean isInclude(String str) {
+
+        Node node = this.head;
+
+        for (char c : str.toCharArray()) {
+            node = node.getChildren().get(c);
+
+            if (node == null) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
