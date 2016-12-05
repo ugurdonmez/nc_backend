e1c22b8b69f303c7ee5448fad430b3ff1dbbd355
ugurdonmez <ugurdonmez87@gmail.com>
2016-12-05 01:18:44 +0100

    comment updated.

--- src/main/java/com/ugurdonmez/email/validation/common/CountryCodeValidator.java
+++ src/main/java/com/ugurdonmez/email/validation/common/CountryCodeValidator.java
@@ -2,6 +2,10 @@ package com.ugurdonmez.email.validation.common;
 
 /**
  *
+ * Interface for control country code in domain
+ * 
+ * ref: http://www.iana.org/domains/root/db
+ * 
  * @author ugurdonmez
  */
 public interface CountryCodeValidator {
