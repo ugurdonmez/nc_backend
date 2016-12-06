4bbc72d89ad14e09c8690f29764dcfb5ae0184a3
ugurdonmez <ugurdonmez87@gmail.com>
2016-12-06 12:51:17 +0100

    DomainValidatorImpl added.

--- src/main/java/com/ugurdonmez/email/validation/basic/common/DomainValidator.java
+++ src/main/java/com/ugurdonmez/email/validation/basic/common/DomainValidator.java
@@ -2,8 +2,19 @@ package com.ugurdonmez.email.validation.basic.common;
 
 /**
  *
+ * Interface for control common domain
+ * 
+ * 
  * @author ugurdonmez
  */
 public interface DomainValidator {
+ 
+    /**
+     * Method to check domain is valid or not
+     * 
+     * @param String domain
+     * @return boolean
+     */
+    boolean isDomainCorrect(String domain);
     
 }
--- src/main/java/com/ugurdonmez/email/validation/basic/common/impl/DomainValidatorImpl.java
+++ src/main/java/com/ugurdonmez/email/validation/basic/common/impl/DomainValidatorImpl.java
@@ -1,11 +1,15 @@
 package com.ugurdonmez.email.validation.basic.common.impl;
 
 import com.ugurdonmez.email.validation.basic.common.DomainValidator;
+import com.ugurdonmez.email.validation.basic.data.DomainsStorage;
 
 /**
  *
  * @author ugurdonmez
  */
 public class DomainValidatorImpl implements DomainValidator{
-    
+
+    public boolean isDomainCorrect(String domain) {
+        return DomainsStorage.getInstance().isIncludeDomain(domain);
+    }
 }
