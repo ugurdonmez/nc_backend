e41e7e683b39fea29ff76b461ddd2b5c1da7fcf0
ugurdonmez <ugurdonmez87@gmail.com>
2016-12-07 20:21:58 +0100

    issue 11 12 - DivideUtil nullPointerException

--- src/main/java/com/ugurdonmez/email/validation/basic/divide/DivideUtil.java
+++ src/main/java/com/ugurdonmez/email/validation/basic/divide/DivideUtil.java
@@ -45,11 +45,8 @@ public class DivideUtil {
         // get public part of domain (top level domain.country code)
         InternetDomainName publicDomainName = fullDomainName.publicSuffix();
         
-        String fd = fullDomainName.toString();
-        String pdn = publicDomainName.toString();
-        
         // return only domain name without top level domain and country code
-        return  fullDomainName.toString().substring(0, fd.length() - pdn.length() - 1);
+        return  fullDomainName.parts().get(0);
     }
     
     /**
@@ -61,6 +58,13 @@ public class DivideUtil {
     public static String getTopLevelDomain(String email) {
         // get full domain name
         InternetDomainName fullDomainName = InternetDomainName.from(email.split("@")[1]);
+        
+        // we have unvalid public suffix
+        if (!fullDomainName.hasPublicSuffix()) {
+            // TODO: now we assume that we dont have subdomain and country code is at the end
+            return fullDomainName.parts().get(1);
+        }
+        
         // get public part of domain (top level domain.country code)
         InternetDomainName publicDomainName = fullDomainName.publicSuffix();
         
@@ -75,6 +79,12 @@ public class DivideUtil {
             
             // first part is country code, so there is no top level domain
             if (countryCodeValidator.isCountryCodeCorrect(publicDomainName.parts().get(0))) {
+                // it is possible that we have non valid top level domain
+                // gmail.comm.tr
+                if (fullDomainName.parts().size() >= 3) {
+                    return fullDomainName.parts().get(fullDomainName.parts().size()-2);
+                }
+                
                 return "";
             } else {
                 return publicDomainName.parts().get(0);
@@ -95,9 +105,22 @@ public class DivideUtil {
     public static String getCountryCode(String email) {
         // get full domain name
         InternetDomainName fullDomainName = InternetDomainName.from(email.split("@")[1]);
+        
+        // we have unvalid public suffix
+        if (!fullDomainName.hasPublicSuffix()) {
+            // TODO: now we assume that we dont have subdomain and country code is at the end
+            if (fullDomainName.parts().size() == 3) {
+                return fullDomainName.parts().get(2);
+            } else {
+                // if we have only two part, one is domain and second is top level domain
+                // TODO: not always true
+                return "";
+            }
+        }
+        
         // get public part of domain (top level domain.country code)
         InternetDomainName publicDomainName = fullDomainName.publicSuffix();
-        
+                
         // there is no any public domain name
         if (publicDomainName.parts().size() == 0) {
             return "";
