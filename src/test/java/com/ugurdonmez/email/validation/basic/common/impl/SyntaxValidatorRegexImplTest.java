95abcb6a962e8903a86311034cd06c00b165ac76
ugurdonmez <ugurdonmez87@gmail.com>
2016-12-07 20:20:36 +0100

    ignore SyntaxValidatorRegexImplTest (not used).

--- src/test/java/com/ugurdonmez/email/validation/basic/common/impl/SyntaxValidatorRegexImplTest.java
+++ src/test/java/com/ugurdonmez/email/validation/basic/common/impl/SyntaxValidatorRegexImplTest.java
@@ -2,11 +2,13 @@ package com.ugurdonmez.email.validation.basic.common.impl;
 
 import com.ugurdonmez.email.validation.basic.common.impl.SyntaxValidatorRegexImpl;
 import com.ugurdonmez.email.validation.basic.common.SyntaxValidatorTest;
+import org.junit.Ignore;
 
 /**
  *
  * @author ugurdonmez
  */
+@Ignore
 public class SyntaxValidatorRegexImplTest extends SyntaxValidatorTest<SyntaxValidatorRegexImpl>{
 
     @Override
